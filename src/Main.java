e669106f23ebc6104e7c7d033495d9fffca2f0de
grgr_smrkv <grgrsmrkv@gmail.com>
2023-03-28 18:38:06 +0300

    added file

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        /*если клиент пополняет счет больше, чем на 1000 рублей,*/
+        /*то оператор дарит ему по 1 рублю за каждые полные 100 рублей*/
+
+        int initialBalance = 600;
+        int amountDeposit = 1000;
+        int bonus;
+
+        if (amountDeposit > 950) {
+            bonus = amountDeposit / 100;
+        } else {
+            bonus = 0;
+        }
+        System.out.println("Счет клиента:" + (initialBalance + amountDeposit + bonus) + "рублей");
+    }
+}
